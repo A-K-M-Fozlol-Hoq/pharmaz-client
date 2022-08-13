02e52d3f725f7b3115d6abc73d22137bdd7bfb6f
A-K-M-Fozlol-Hoq <akmfozlolhoq@gmail.com>
2022-08-13 16:23:06 +0600

    test commit

---
+++ components/Utils/returnHello.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function ReturnHello() {
+  return <div>returnHello</div>;
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
+import ReturnHello from '../components/Utils/returnHello';
 import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
@@ -14,6 +15,7 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className="bg-slate-600 text-center">Hello world</h1>
+        <ReturnHello />
       </main>
 
       <footer className={styles.footer}>
