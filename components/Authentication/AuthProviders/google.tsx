621b4f49fb2b70038049b7848886268fdc537784
A-K-M-Fozlol-Hoq <akmfozlolhoq@gmail.com>
2022-09-30 10:41:16 +0600

    tailwind directive added

--- components/Authentication/AuthProviders/google.tsx
+++ components/Authentication/AuthProviders/google.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import { handleGoogleSignIn } from '../authManager';
 export default function GoogleAuthProvider() {
-  return <div onClick={handleGoogleSignIn}>LOGIN WITH GOOGLE</div>;
+  return (
+    <div className="btn btn-primary py-4" onClick={handleGoogleSignIn}>
+      LOGIN WITH GOOGLE
+    </div>
+  );
 }
